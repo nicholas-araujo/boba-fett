6a311b072f7a96cec3a198941c7081a5dc731995
m4rd <nicholas-araujo@users.noreply.github.com>
2016-03-09 00:51:50 +0000

    created package.json, procfile and start script

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// "www" is considered as the public folder in your app to serve the static resources
+app.use(express.static(path.join(__dirname, 'www')));
+
+// This is necessary to specify the port from process.env
+// Otherwise heroku will throw an error
+app.listen(process.env.PORT || 5000);
+console.log("Application server started...");
